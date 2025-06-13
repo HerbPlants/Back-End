bc1e0e7a99ca7f949ef3e84a07cb0395d4277514
Zidan <maulanazidan12@gmail.com>
2025-06-13 11:06:30 +0700

    feat:tambahkan seed agar app bisa jalan

--- data-db/seed.js
+++ data-db/seed.js
@@ -2,31 +2,54 @@ const { PrismaClient } = require("@prisma/client");
 const fs = require("fs");
 
 const seed = async () => {
-  let prisma;
+  const prisma = new PrismaClient();
 
   try {
-    prisma = new PrismaClient();
-
     await prisma.$connect();
     console.log("✅ Terhubung ke database");
 
-    const data = JSON.parse(
-      fs.readFileSync("./data-db/hasil-scraping.json", "utf-8")
+    const herbData = JSON.parse(
+      fs.readFileSync("./data-db/data-herb.json", "utf-8")
     );
 
-    for (const herb of data) {
+    for (const herb of herbData) {
       await prisma.herb.upsert({
         where: { name: herb.name },
         update: {},
         create: herb,
       });
     }
+    console.log("✅ Seeding table `herb` selesai");
 
-    console.log("✅ Seeding selesai");
+    const bestHerbData = JSON.parse(
+      fs.readFileSync("./data-db/data-bestHerb.json", "utf-8")
+    );
+
+    for (const item of bestHerbData) {
+      await prisma.bestHerb.upsert({
+        where: { herbId: item.herbId },
+        update: {
+          name: item.name,
+          khasiat: item.khasiat,
+          image: item.image,
+          updatedAt: new Date(item.updatedAt),
+        },
+        create: {
+          herbId: item.herbId,
+          name: item.name,
+          khasiat: item.khasiat,
+          image: item.image,
+          createdAt: new Date(item.createdAt),
+          updatedAt: new Date(item.updatedAt),
+        },
+      });
+    }
+    console.log("✅ Seeding table `bestHerb` selesai");
+
   } catch (err) {
-    console.error("❌ Gagal insert atau koneksi DB:", err.message);
+    console.error("❌ Gagal insert:", err.message);
   } finally {
-    if (prisma) await prisma.$disconnect();
+    await prisma.$disconnect();
   }
 };
 
